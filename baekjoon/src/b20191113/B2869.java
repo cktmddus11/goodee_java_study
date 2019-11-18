6bd4ede955d1632414732a250d0c03c9cfd7ed85
CSY <cktmddus11@naver.com>
2019-11-18 17:21:46 +0900

    jquery3

---
+++ baekjoon/src/b20191113/B2869.java
@@ -0,0 +1,17 @@
+package b20191113;
+
+import java.util.Scanner;
+
+public class B2869 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int up = scan.nextInt(); 
+		int down = scan.nextInt(); 
+		int max = scan.nextInt(); 
+		
+		if((max -  (up + down))  % 2== 0) {
+			System.out.println(max )
+		}
+		
+	}
+}
